a28f68d353b4e1c497157a487bf049514092b156
Tyler Brothers <tylerbrothers1@gmail.com>
2015-10-29 13:21:34 -0700

    exchanging, setComponent manually

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var s = require('./lib/standard.js');
+var s = require('./lib/interface.js');
 
 s.emitter.on('exchange', function(id){
   s.trigger(1, 'exchange share 0');
 });
 s.addEntity(0);
-s.addEntity(1);
-s.trigger(0, 'exchange share 1');
+s.setComponent(0, 'position', [0,0]);
+s.addEntity('tree');
+s.setComponent('tree', 'apples', 5);
+s.setComponent('tree', 'position', [0,0]);
+s.trigger(0, 'gather');
 console.log(s.tick());
