3775effc4116580a92814e53cc95cae517e9acb3
Tyler Brothers <tylerbrothers1@gmail.com>
2015-10-31 15:08:10 -0700

    new: package.json, index now exports Interface. fix: getComponents returns copy of components.

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-var s = require('./lib/interface.js');
-
-s.addEntity(0);
-console.log(s.getComponents(0));
-
-s.addEntity('tree');
-s.addEntity('tree');
-s.setComponent('tree', 'not', 'unique');
-console.log(s.tick());
+module.exports = require('./lib/interface.js');
