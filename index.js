f8efac6724dc646b4e361b953f2ec685d54ad6a2
Tyler Brothers <tylerbrothers1@gmail.com>
2015-10-30 19:09:09 -0700

    interface now has private functions but public properties, getComponents

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 var s = require('./lib/interface.js');
 
-s.emitter.on('exchange', function(id){
-  s.trigger(1, 'exchange share 0');
-});
 s.addEntity(0);
-s.setComponent(0, 'position', [0,0]);
+console.log(s.getComponents(0));
+
 s.addEntity('tree');
-s.setComponent('tree', 'apples', 5);
-s.setComponent('tree', 'position', [0,0]);
-s.trigger(0, 'gather');
+s.addEntity('tree');
+s.setComponent('tree', 'not', 'unique');
 console.log(s.tick());
